937eaf7ff4a73bdffb4b70203e65dea6f179dd8b
Jaden <jadentr.44@gmail.com>
2022-09-29 13:08:36 -0500

    vercel error

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import Skill from "../components/skills/Skills"
-import Projects from "../components/projects/projects"
+import Skill from "../components/skills/Skills";
+import Projects from "../components/projects/Projects";
+
 export default function Home() {
   return (
     <div className=" z-20">
