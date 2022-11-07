69ff8a950b81deaadda417fa6b5c715227dd7b4d
Jaden <jadentr.44@gmail.com>
2022-11-07 17:12:58 -0600

    updated link

--- components/nav/Nav.jsx
+++ components/nav/Nav.jsx
@@ -166,7 +166,7 @@ export default function Nav({
           <Projects />
         </div>
         {console.log("if you are seeing this you must REALLY like this...or something broke")}
-     {console.log('https://www.youtube.com/watch?v=a3Z7zEc7AXQ')}
+     {console.log('https://www.youtube.com/watch?v=jz5a-KYJY3M')}
      {console.log("since you are here you might as well check out this video")}
         <div
           onClick={() => {
