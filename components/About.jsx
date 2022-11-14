11d9535c84033968780f23c7492e637ea8abce7b
Jaden <jadentr.44@gmail.com>
2022-11-14 14:48:09 -0600

    mobile about me layout

--- components/About.jsx
+++ components/About.jsx
@@ -4,15 +4,16 @@ import {FaChessKnight,FaRunning,FaMusic} from 'react-icons/fa'
 import {IoLogoGameControllerA} from 'react-icons/io'
 export default function About() {
   return (
-    <div className="px-[10%] pt-48 flex " id="about">
-      <div className="w-1/3  ">
+    <div className="lg:px-[10%] px-[5%] pt-10 lg:pt-48 flex  " id="about">
+      <div className="w-1/3 lg:block hidden  ">
         <img src="/me.svg" alt="" />
       </div>
-      <div className="w-full h-auto  pl-[10%]">
-        <div className="flex items-center">
+      <div className="lg:w-2/3 h-auto  lg:pl-[10%]">
+        <div className="flex  items-center">
           <div className="h-1 w-full bg-black"></div>
         <h1 className=" mx-2 text-4xl   ">About<span className="mx-2"></span>Me</h1>
           <div className="h-1 w-full bg-black"></div>
+          <img className="w-1/3 block lg:hidden lg:w-0" src="/me.svg" alt="" />
         </div>
         <div className="text-xl mt-5 ">
           <p className="">
@@ -25,20 +26,20 @@ export default function About() {
           
         </div>
         <h3 className="text-2xl mt-5">Some of my interest outside of Coding:</h3>
-        <div className="flex mt-5 justify-around w-[80%]">
-          <div className="border-2 h-32 w-32 bg-red-500 rounded-2xl text-center flex flex-col justify-around items-center text-white">
+        <div className="flex mt-5 justify-around lg:w-[80%]">
+          <div className="border-2 lg:h-32 lg:w-32 w-[20%] h-auto bg-red-500 rounded-2xl text-center flex flex-col justify-around items-center text-white">
             <p className="text-lg text-white">Chess</p>
             < FaChessKnight size={"50%"} />
           </div>
-          <div className="border-2 h-32 w-32 bg-red-500 rounded-2xl text-center flex flex-col justify-around items-center text-white">
+          <div className="border-2 lg:h-32 lg:w-32 w-[20%] h-auto bg-red-500 rounded-2xl text-center flex flex-col justify-around items-center text-white">
             <p className="text-lg text-white">Fitness</p>
             < FaRunning size={"50%"} />
           </div>
-          <div className="border-2 h-32 w-32 bg-red-500 rounded-2xl text-center flex flex-col justify-around items-center text-white">
+          <div className="border-2 lg:h-32 lg:w-32 w-[20%] h-auto bg-red-500 rounded-2xl text-center flex flex-col justify-around items-center text-white">
             <p className="text-lg text-white">Music</p>
             < FaMusic size={"50%"} />
           </div>
-          <div className="border-2 h-32 w-32 bg-red-500 rounded-2xl text-center flex flex-col justify-around items-center text-white">
+          <div className="border-2 lg:h-32 lg:w-32 w-[20%] h-auto bg-red-500 rounded-2xl text-center flex flex-col justify-around items-center text-white">
             <p className="text-lg text-white">Gaming</p>
             < IoLogoGameControllerA size={"50%"} />
           </div>
